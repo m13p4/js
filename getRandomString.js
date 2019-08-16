58a92cf4603224a7cb59295b61c6d3813ccf3332
m13p4 <pavel@wtnet.de>
2019-08-16 20:20:26 +0200

    Update getRandomString.js

--- getRandomString.js
+++ getRandomString.js
@@ -1,16 +1,16 @@
 /**
- * Random string 
- * 
+ * Get a random string
+ *
  * @version 1.0
  * @author m13p4
  */
-function getRandomString(length)
+function getRandomString(len)
 {
     var str = "";
     
-    while(str.length < (length||16))
+    while(str.length < (len||16))
         str += Math.random().toString(36).substr(2);
     
-    return str.substr(0, length);
+    return str.substr(0, (len||16));
 }
-typeof module !== "undefined" && (module.exports = getRandomString);
+typeof module != "undefined" && (module.exports = getRandomString);
