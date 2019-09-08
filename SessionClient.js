789a7b241943a59847339db773f23dea6ad1efd1
m13p4 <pavel@wtnet.de>
2019-09-08 19:55:17 +0200

    Create SessionClient.js

---
+++ SessionClient.js
@@ -0,0 +1,47 @@
+/**
+ * A Client for a Session Server.
+ * 
+ * @version 1.0
+ *
+ * @author m13p4
+ * @copyright Meliantchenkov Pavel
+ */
+
+(function(___)
+{
+    var SessionClient = {
+        conf: null,
+        client: null,
+        
+        connect: function()
+        {
+            
+        },
+        new: function(hash, exp, length)
+        {
+            
+        },
+        check: function(hash, sid)
+        {
+            
+        },
+        close: function(hash, sid)
+        {
+            
+        },
+        set: function(hash, sid, key, val)
+        {
+            
+        },
+        get: function(hash, sid, key)
+        {
+            
+        },
+        ping: function()
+        {
+            
+        }
+    };
+    ___.exports = SessionClient;
+    
+})(typeof module !== "undefined" ? module : {});
